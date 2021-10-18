607be59f46f8dfd88c3d3e16588774eac082e5ab
ShriyanshChoudhury <74601230+ShriyanshChoudhury@users.noreply.github.com>
2021-10-18 17:37:58 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,41 @@
+var City
+var boy
+var BoyRunning
+var Obstacle
+
+function preload(){
+  City =loadImage("City Bg For School Runner Game.png");
+  Running = loadImage("Boy_Running.PNG")
+  Obstacle = loadImage("Barricade.png")
+}
+
+
+
+
+function setup() {
+  createCanvas(2500,635);
+  Bg = createSprite(770,300,2500,1080)
+  Bg.addImage(City)
+  Boy = createSprite(200,530,20,50);
+  Boy.addImage(Running)
+  Boy.scale=0.35
+  Bar = createSprite(700,580,20,50);
+  Bar.addImage(Obstacle)
+  Bar.scale=0.225
+  Bg.scale=1.0
+  Ground = createSprite(635,633,2500,5)
+  
+
+  
+
+}
+
+function draw() {
+  background("black"); 
+  Boy.collide(Ground)
+  if(keyDown("space") && Boy.y >=  500 ) {
+    Boy.velocityY = -12;
+  }
+  Boy.velocityY = Boy.velocityY + 0.8
+  drawSprites();
+}
